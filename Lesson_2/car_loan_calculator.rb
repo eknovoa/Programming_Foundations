3ce3e56c37e5d101f564f2eaa1812caf0b7936a3
Erin Novoa <erinnovoa@gmail.com>
2020-08-19 09:45:12 -0400

    These are the edited files from the first code review request.

--- Lesson_2/car_loan_calculator.rb
+++ Lesson_2/car_loan_calculator.rb
@@ -7,22 +7,19 @@ end
 
 def monthly_int_rate(rate)
   int_rate = (rate.to_f() / 12) / 100
-
   int_rate
 end
 
 def loan_dur_mths(loan_time)
   loan_time_mths = loan_time.to_f() * 12
-
   loan_time_mths
 end
 
-def calculator(amount, rate, loan_time)
+def calculator(amount, apr_rate, loan_time)
   amount = amount.to_f()
-  rate = rate.to_f()
+  apr_rate = apr_rate.to_f()
   loan_time = loan_time.to_f()
-  monthly_pmt = amount * (rate / (1 - (1 + rate)**(-loan_time)))
-
+  monthly_pmt = amount * (apr_rate / (1 - (1 + apr_rate)**(-loan_time)))
   monthly_pmt.round(2)
 end
 
@@ -33,42 +30,54 @@ def clear_screen(answer_again)
   answer_again
 end
 
-def valid_num(num)
+def valid_name?(n)
+  char_num = "0-9!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
   loop do
-    if num.empty?()
-      prompt(MESSAGES['valid_number'])
-      num = Kernel.gets().chomp()
-    else
+    if n.count('a-zA-z') > 0 && n.count(char_num) == 0
       break
+    elsif n.count(char_num) > 0 || n.empty?() || n.count("a-zA-Z") > 0
+      prompt(MESSAGES['invalid_number'])
+      n = Kernel.gets().chomp()
     end
+  end
+  n
+end
 
-    if num.match?(/\A-?\d+\Z/)
+def valid_loan_amt?(l)
+  char_lett = "a-zA-Z!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  loop do
+    if l.to_i > 0 && l.count(char_lett) == 0
       break
-    else
-      prompt(MESSAGES['valid_number'])
+    elsif l.empty?() || l.count(char_lett)
+      prompt(MESSAGES['invalid_number'])
+      l = Kernel.gets().chomp()
+    end
+  end
+  l
+end
+
+def valid_apr_dur?(num)
+  char_lett = "a-zA-Z!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  loop do
+    if num.to_i.between?(1, 100) && num.count(char_lett) == 0
+      break
+    elsif num.count("0-9") > 0 || num.count(char_lett) > 0 || num.empty?()
+      prompt(MESSAGES['invalid_number'])
       num = Kernel.gets().chomp()
     end
   end
   num
 end
 
-def valid_name(first_name)
-  loop do
-    if first_name.empty?()
-      prompt(MESSAGES['valid_name'])
-      first_name = Kernel.gets().chomp()
-    else
-      break
-    end
-
-    if first_name.match?(/\A-?\d+\Z/)
-      prompt(MESSAGES['valid_name'])
-      first_name = Kernel.gets().chomp()
-    else
-      break
-    end
+def valid_input?(input, input_type)
+  case input_type
+  when "loan_amt"
+    valid_loan_amt?(input)
+  when "apr"
+    valid_apr_dur?(input)
+  when "duration"
+    valid_apr_dur?(input)
   end
-  first_name
 end
 
 prompt(MESSAGES['welcome'])
@@ -76,33 +85,51 @@ prompt(MESSAGES['welcome'])
 loop do
   prompt(MESSAGES['first_name'])
   name = Kernel.gets().chomp()
-  name = valid_name(name)
+  name = valid_name?(name)
   prompt("Hi, #{name.capitalize}!")
 
   prompt(MESSAGES['retrieve_info'])
   prompt(MESSAGES['confirmation'])
   answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
 
-  prompt(MESSAGES['loan_amount'])
+  if answer != 'y' && answer != 'n'
+    loop do
+      prompt(MESSAGES['invalid_response'])
+      answer = Kernel.gets().chomp().downcase()
+      break if answer.start_with?('y') && answer.size == 1
+      break if answer.start_with?('n') && answer.size == 1
+    end
+  end
+  break unless answer.start_with?('y') && answer.size == 1
+
+  prompt(MESSAGES['loan_amt'])
   amount = Kernel.gets().chomp()
-  amount = valid_num(amount)
+  amount = valid_input?(amount, "loan_amt")
 
   prompt(MESSAGES['percentage_rate'])
   apr = Kernel.gets().chomp()
-  apr = valid_num(apr)
+  apr = valid_input?(apr, "apr")
 
   prompt(MESSAGES['loan_duration'])
   loan_duration = Kernel.gets().chomp()
-  loan_duration = valid_num(loan_duration)
+  loan_duration = valid_input?(loan_duration, "duration")
 
-  rate = monthly_int_rate(apr)
+  apr_rate = monthly_int_rate(apr)
   loan_dur = loan_dur_mths(loan_duration)
-  prompt("Your monthly payment is: $#{calculator(amount, rate, loan_dur)}.")
+  prompt("Your monthly payment is: $#{calculator(amount, apr_rate, loan_dur)}.")
 
   prompt(MESSAGES['another_calc'])
   answer_again = Kernel.gets().chomp()
-  break unless answer_again.downcase().start_with?('y')
+
+  if answer_again != 'y' && answer_again != 'n'
+    loop do
+      prompt(MESSAGES['invalid_response'])
+      answer_again = Kernel.gets().chomp().downcase()
+      break if answer_again.start_with?('y') && answer_again.size == 1
+      break if answer_again.start_with?('n') && answer_again.size == 1
+    end
+  end
+  break unless answer_again.start_with?('y') && answer_again.size == 1
   clear_screen(answer_again)
 end
 
