cdf2d435e5af7f83b71dde7f37fcd9e2a4e59c2c
Erin Novoa <erinnovoa@gmail.com>
2020-08-30 21:43:23 -0400

    updated

--- Small_Problems/easy_2/even_numbers.rb
+++ Small_Problems/easy_2/even_numbers.rb
@@ -1,4 +1,4 @@
-# Print the even numberes from 1 to 99, inclusive. All numbers should be printed on separate lines.
+# Print the even numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
 
 for num in (1..99)
   puts num if num.even?
