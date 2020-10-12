0cba1d03656a26d94a104c40395f6df04645f682
Erin Novoa <erinnovoa@gmail.com>
2020-10-11 21:56:39 -0400

    updated array_average file.

--- Small_Problems/array_average.rb
+++ Small_Problems/array_average.rb
@@ -1,16 +1,39 @@
+=begin
+Problem
+-write a method that takes one argument, an array containing integers, and returns the average
+of all numbers in the array.
+
+  Input:
+  -array of integers
+  Output:
+  -integer
+
+  Rules:
+  -the array will never be empty
+  -the numbers will always be positive integers
+  -your result should also be an integer
+
+Examples/Test cases
+-puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3     => true
+-puts average([1, 5, 87, 45, 8, 8]) == 25                           => true
+-puts average([9, 47, 23, 95, 16, 52]) == 40                        => true
+
+Data Structure
+-arrays and integers
+
+Algorithm
+-define the method average and it has one parameter, array
+-call the sum method on the parameter, array and divide that by calling the size method on the parameter, array
+
+Code
+
+=end
+
 def average(array)
-  sum = 0
-  array.each do |element|
-    sum = sum + element
-  end
-  avg = sum.to_f / (array.size).to_f
-  avg
+  array.sum/array.size
 end
 
-# test cases
-puts average([1, 6])
-puts average([1, 5, 87, 45, 8, 8])
-puts average([9, 47, 23, 95, 16, 52])
-
-
-# also could have used Enumerable#reduce (#inject) // array.reduce(:+)
+#test cases
+p average([1, 6]) == 3
+p average([1, 5, 87, 45, 8, 8]) == 25
+p average([9, 47, 23, 95, 16, 52]) == 40
