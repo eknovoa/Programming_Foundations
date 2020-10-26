52440a186a410c8a588809195aab5c5b7f9f8745
Erin Novoa <erinnovoa@gmail.com>
2020-10-26 18:24:40 -0400

    Refactored.

--- Small_Problems/easy_2/odd_numbers.rb
+++ Small_Problems/easy_2/odd_numbers.rb
@@ -1,7 +1,5 @@
 # Print all odd numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
 
 for num in (1..99)
-  if num.odd?
-    puts num
-  end
+  puts num if num.odd?
 end
